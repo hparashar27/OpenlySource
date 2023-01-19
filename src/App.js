db90ff7fe81bd14f09d9f9451308b5722466ce64
Harsh <hparashar617@gmail.com>
2023-01-19 13:34:54 +0530

    setup complete

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+    <h1>  Welcome to OpenlySource ......</h1>
+      <h2>Start your contribution 🚀</h2>
+    </div>
+  );
+}
+
+export default App;
