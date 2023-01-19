ccf697941dbee8f771ed179cc1bf587a2ad6c10d
Harsh <hparashar617@gmail.com>
2023-01-19 15:00:44 +0530

    addition of logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css"
+import logo from "./assets/Official_openlySource_logo.png"
 
 function App() {
   return (
     <div className="App">
+       <img src={logo}/>
     <h1>  Welcome to OpenlySource ......</h1>
-      <h2>Start your contribution 🚀</h2>
+      <h2>Start your contribution </h2>
     </div>
   );
 }
