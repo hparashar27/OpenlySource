874f11ad7624b39727b20bbd8e78308dff0b7108
Harsh <hparashar617@gmail.com>
2023-01-22 19:13:58 +0530

    removal of undercontruction file

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,3 @@
 export {default as About} from './About/About'
 export {default as Navbar} from './Navbar/Navbar'
-export {default as UnderConstruction} from './Construction/UnderConstruction'
 export {default as Header} from './Header/Header'
