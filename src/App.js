b9d4284a4eb052ce6411279971300f39496b51c7
Nikhil <nikhilsnayak3473@gmail.com>
2023-01-25 15:29:25 +0530

    Added Footer Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import "./App.css"
-import { Navbar,Header } from "./Components";
+import './App.css';
+import { Navbar, Header, Footer } from './Components';
 // import from "./Components/Header/About";
-import { BrowserRouter as Router,  } from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <div className="gradient_bg">
- <Router>
-      <Navbar/>
-      <Header/> 
-</Router>
-    </div>
+    <div className='App'>
+      <div className='gradient_bg'>
+        <Router>
+          <Navbar />
+          <Header />
+          <Footer />
+        </Router>
+      </div>
     </div>
   );
 }
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,3 +1,4 @@
-export {default as About} from './About/About'
-export {default as Navbar} from './Navbar/Navbar'
-export {default as Header} from './Header/Header'
+export { default as About } from './About/About';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as Header } from './Header/Header';
+export { default as Footer } from './Footer/Footer';
