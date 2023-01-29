ebc2ac75aac8dc39431a6d5e719df1b7770c4936
sumeet <sumeetmaurya135@gmail.com>
2023-01-29 23:30:21 +0530

    Added login and signuop pages

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ import Opportunities from './Components/Opportunities/Opportunities';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
 import UnderConstruction from './Components/UnderConstruction/UnderConstruction';
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup"
+
 
 function App() {
   return (
         <Router>
+          <Route exact path="/OpenlySource">
           <Navbar/>
+        </Route>
           <Switch>
         <Route exact path="/">
           <Home/>
@@ -24,6 +29,8 @@ function App() {
         <Route exact path="/Members">
         <UnderConstruction/>
         </Route>
+        <Route exact path="/Log-in" component={Login} />
+        <Route exact path="/Sign-up" component={Signup} />
           </Switch>
         </Router>
   );
