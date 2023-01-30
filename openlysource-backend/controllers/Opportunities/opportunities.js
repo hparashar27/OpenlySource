16dcebb7b13dec5b393508398051ecdb7ae672e3
Faizan-omer <omerfaizan1@gmail.com>
2023-01-30 22:27:55 +0500

    added post event api

--- openlysource-backend/controllers/Opportunities/opportunities.js
+++ openlysource-backend/controllers/Opportunities/opportunities.js
@@ -1,11 +1,44 @@
-const getOpportunitiesService = require("../../services/Opportunities/get");
-const { successResponse } = require("../../utils/response.utils");
+const opportunitiesServices = require("../../services/Opportunities");
+const { successResponse, serverErrorResponse } = require("../../utils/response.utils");
 
 const getEvents = async (req, res, next) => {
   try {
-    const responseData = await getOpportunitiesService.getEvents();
+    const responseData = await opportunitiesServices.getEvents();
 
     successResponse(res, responseData);
+    return;
+  } catch (error) {
+    next(error);
+  }
+};
+
+const addEvent = async (req, res, next) => {
+  try {
+    const { months, description, eventName, logo, websiteUrl } = req.body;
+
+    let event = await opportunitiesServices.addEvent(
+      months,
+      description,
+      eventName,
+      logo,
+      websiteUrl,
+    );
+
+    if (!event) {
+      serverErrorResponse(res);
+      return;
+    }
+
+    event = {
+      months: event.months,
+      description: event.description,
+      logo: event.logo,
+      websiteUrl: event.websiteUrl,
+      eventName: event.eventName,
+    };
+
+    successResponse(res, event);
+    return;
   } catch (error) {
     next(error);
   }
@@ -13,4 +46,5 @@ const getEvents = async (req, res, next) => {
 
 module.exports = {
   getEvents,
+  addEvent,
 };
