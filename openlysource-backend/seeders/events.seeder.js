cf248efbf3195d05e9b0095364eb5a752973cc1e
Faizan-omer <omerfaizan1@gmail.com>
2023-01-30 22:29:10 +0500

    added logo in seeder

--- openlysource-backend/seeders/events.seeder.js
+++ openlysource-backend/seeders/events.seeder.js
@@ -4,7 +4,7 @@ const { Event } = require("../models");
 const data = [
   {
     eventName: "Google Summer of Code",
-    logo: "",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/Google_Summer_of_Code_sun_logo_2022.svg/1200px-Google_Summer_of_Code_sun_logo_2022.svg.png",
     description:
       "Google Summer of Code is a global, online program focused on bringing new contributors into open source software development. GSoC Contributors work with an open source organization on a 12+ week programming project under the guidance of mentors",
     websiteUrl: "https://summerofcode.withgoogle.com/",
