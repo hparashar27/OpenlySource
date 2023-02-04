6a2a328fa52b0fd4b57330303a9e268b43cb043f
lokeshvasnik <lokeshvasnik2003@gmail.com>
2023-02-04 17:28:10 +0530

    Update font family and updated react-router
    React router module from version 5 to 6 & tweaked the font family

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-import './App.css';
-// import { Navbar, Header, Footer } from './Components';
-// import from "./Components/Header/About";
-import { BrowserRouter as Router, Route ,Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Opportunities from './Components/Opportunities/Opportunities';
 import Home from './Components/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
 import UnderConstruction from './Components/UnderConstruction/UnderConstruction';
-import Login from "./Components/Login/Login";
-import Signup from "./Components/Signup/Signup"
-
+import Login from './Components/Login/Login';
+import Signup from './Components/Signup/Signup';
+import './App.css';
 
 function App() {
-  return (
-    <Router>
-    <Navbar/>
-    <Switch>
-  <Route exact path="/openlysource">
-    <Home/>
-  </Route>  
-  <Route exact path="/Opportunities">
-  <Opportunities/>
-  </Route>
-  <Route exact path="/Aboutus">
-  <UnderConstruction/>
-  </Route>
-  <Route exact path="/Members">
-  <UnderConstruction/>
-  </Route>
-    </Switch>
-    <Route exact path="/Log-in" component={Login} />
-        <Route exact path="/Sign-up" component={Signup} />
-  </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Opportunities" element={<Opportunities />} />
+                <Route path="/Aboutus" element={<UnderConstruction />} />
+                <Route path="/Members" element={<UnderConstruction />} />
+                <Route path="/Log-in" element={<Login />} />
+                <Route path="/Sign-up" element={<Signup />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
