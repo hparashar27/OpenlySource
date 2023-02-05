71379fd517b2607a4bf5967ec450ee090ef2e7c9
Harsh <hparashar617@gmail.com>
2023-02-05 17:58:20 +0530

    setting up of the opportunities page

--- src/Components/Opportunities/Opportunities.js
+++ src/Components/Opportunities/Opportunities.js
@@ -26,7 +26,7 @@ fecthData();
         {/* for Google summer of code */}
         {Events && Events.map && Events.map((Event,_id) => <div key={_id} className="Opp_card">
           <div className="card_left ">
-            <img src={Event.logo} height="30%" width="30%" />
+            <img src={Event.logo} className="event_logo" />
           </div>
           <div className="card_right">
             <span className="card_right_heading">{Event.eventName}</span>
@@ -44,48 +44,6 @@ fecthData();
             </div>
           </div>
         </div>)}
-        {/* for MLH fellowship */}
-        {/* <div className="Opp_card">
-          <div className="card_left ">
-            <img src={mlh} height="70%" width="60%" />
-          </div>
-          <div className="card_right">
-            <span className="card_right_heading"> MLH Fellowship </span>
-            <span className="card_right_description">
-              {" "}
-              MLH Fellowship is a program to contribute to open source projects.A fully remote, 12-week internship where participants earn a stipend and learn to collaborate on real open source projects with peers and engineers from top companies.
-            </span>
-            <div className="card_right_button">
-              <button className="visit_button"> <a
-                rel='noreferrer'
-                href='https://fellowship.mlh.io/'
-                target='_blank'
-              ><TbLink/>Visit</a></button>
-              <span className="month_name">Jan-May</span>
-            </div>
-          </div>
-        </div> */}
-         {/* for Summer of bitcoins */}
-         {/* <div className="Opp_card">
-          <div className="card_left ">
-            <img src={sob} height="70%" width="50%" />
-          </div>
-          <div className="card_right">
-            <span className="card_right_heading"> Summer of Bitcoin </span>
-            <span className="card_right_description">
-              {" "}
-              A global, online summer internship program focused on introducing university students to bitcoin open-source development and design
-            </span>
-            <div className="card_right_button">
-              <button className="visit_button"><a
-                rel='noreferrer'
-                href='https://www.summerofbitcoin.org/'
-                target='_blank'
-              ><TbLink/>Visit</a></button>
-              <span className="month_name">Jan-April</span>
-            </div>
-          </div>
-        </div> */}
       </div>
     </div>
   );
