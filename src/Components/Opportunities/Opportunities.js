e1ab4cedb98202517b83c18492f1b2d85e67b894
Harsh <hparashar617@gmail.com>
2023-02-05 12:19:26 +0530

    calling apis from backend

--- src/Components/Opportunities/Opportunities.js
+++ src/Components/Opportunities/Opportunities.js
@@ -1,60 +1,51 @@
-import React from "react";
-import "./Opportunities.css";
+import { useEffect,useState } from "react"; 
 import mlh from "../../assets/mlh_logo.jpg";
 import gsoc from "../../assets/gsoc_logo.svg";
 import sob from "../../assets/sob_logo.png"
 import {TbLink} from "react-icons/tb"
+import axios from "axios"
+import "./Opportunities.css";
 
 const Opportunities = () => {
+  const [Events , setEvents ] = useState([]);
+  useEffect(() => {
+  const fecthData = async () => {
+    await axios.get("http://localhost:8000/api/opportunities/events")
+        .then(res => {
+            setEvents(res.data.data);
+            console.log(Events);
+        });
+};
+fecthData();
+},[])
   return (
     <div className="Opp_body">
       {/* opportunities heading  */}
       <div className="Opp_heading">All Upcoming open-source opportunities</div>
       <div className="Opp_cards_list">
         {/* for Google summer of code */}
-        <div className="Opp_card">
+        {Events && Events.map && Events.map((Event,_id) => <div key={_id} className="Opp_card">
           <div className="card_left ">
-            <img src={gsoc} height="70%" width="50%" />
+            <img src={Event.logo} height="30%" width="30%" />
           </div>
           <div className="card_right">
-            <span className="card_right_heading"> Google Summer of Code</span>
+            <span className="card_right_heading">{Event.eventName}</span>
             <span className="card_right_description">
               {" "}
-              Google Summer of Code is a global, online program focused on bringing new contributors into open source software development. GSoC Contributors work with an open source organization on a 12+ week programming project under the guidance of mentors
+          {Event.description}
             </span>
             <div className="card_right_button">
               <button className="visit_button"> <a
                 rel='noreferrer'
-                href='https://summerofcode.withgoogle.com/'
+                href={Event.websiteUrl}
                 target='_blank'
               ><TbLink/>Visit</a></button>
-              <span className="month_name">Jan-April</span>
+              <span className="month_name">{Event.months}</span>
             </div>
           </div>
-        </div>
-        {/* for Summer of bitcoins */}
-        <div className="Opp_card">
-          <div className="card_left ">
-            <img src={sob} height="70%" width="50%" />
-          </div>
-          <div className="card_right">
-            <span className="card_right_heading"> Summer of Bitcoin </span>
-            <span className="card_right_description">
-              {" "}
-              A global, online summer internship program focused on introducing university students to bitcoin open-source development and design
-            </span>
-            <div className="card_right_button">
-              <button className="visit_button"><a
-                rel='noreferrer'
-                href='https://www.summerofbitcoin.org/'
-                target='_blank'
-              ><TbLink/>Visit</a></button>
-              <span className="month_name">Jan-April</span>
-            </div>
-          </div>
-        </div>
+        </div>)}
         {/* for MLH fellowship */}
-        <div className="Opp_card">
+        {/* <div className="Opp_card">
           <div className="card_left ">
             <img src={mlh} height="70%" width="60%" />
           </div>
@@ -73,7 +64,28 @@ const Opportunities = () => {
               <span className="month_name">Jan-May</span>
             </div>
           </div>
-        </div>
+        </div> */}
+         {/* for Summer of bitcoins */}
+         {/* <div className="Opp_card">
+          <div className="card_left ">
+            <img src={sob} height="70%" width="50%" />
+          </div>
+          <div className="card_right">
+            <span className="card_right_heading"> Summer of Bitcoin </span>
+            <span className="card_right_description">
+              {" "}
+              A global, online summer internship program focused on introducing university students to bitcoin open-source development and design
+            </span>
+            <div className="card_right_button">
+              <button className="visit_button"><a
+                rel='noreferrer'
+                href='https://www.summerofbitcoin.org/'
+                target='_blank'
+              ><TbLink/>Visit</a></button>
+              <span className="month_name">Jan-April</span>
+            </div>
+          </div>
+        </div> */}
       </div>
     </div>
   );
