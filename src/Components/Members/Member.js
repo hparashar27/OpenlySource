55898a83db8aa44006b056203a2dbd4554b43783
lokeshvasnik <lokeshvasnik2003@gmail.com>
2023-02-06 12:35:25 +0530

    Members page completed
    Used react-multi-carousel package for the member carousel

--- src/Components/Members/Member.js
+++ src/Components/Members/Member.js
@@ -1,30 +1,110 @@
 import React from 'react';
 import profile from '../../assets/images/profile.png';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import './Member.css';
 const Member = () => {
+    const responsive = {
+        superLargeDesktop: {
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5,
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
     return (
-        <form action="" className="box">
-            <div className="header">
-                <img src={profile} className="profile" alt="" />
-                <button className="primary-btn">Change Image</button>
-                <button className="secondary-btn">Remove</button>
-            </div>
-            <div className="middle">
-                <input className="input" type="text" placeholder="Name" />
-                <input
-                    className="input"
-                    type="text"
-                    placeholder="Github Link"
-                />
-                <textarea
-                    rows="4"
-                    cols="50"
-                    className="input-textarea"
-                    placeholder="Share your thoughts about Open-source"
-                ></textarea>
-                <button className="primary-btn">Become a member</button>
-            </div>
-        </form>
+        <>
+            <form action="" className="box">
+                <div className="header">
+                    <img src={profile} className="header__profile" alt="" />
+                    <button
+                        type="file"
+                        id="myFile"
+                        name="filename"
+                        className="header__btn header__btn--primary"
+                    >
+                        Change Image
+                    </button>
+                    <button className="header__btn header__btn--secondary">
+                        Remove
+                    </button>
+                </div>
+                <div className="middle">
+                    <input className="input" type="text" placeholder="Name" />
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="Github Link"
+                    />
+                    <textarea
+                        rows="4"
+                        cols="50"
+                        className="input-textarea"
+                        placeholder="Share your thoughts about Open-source"
+                    ></textarea>
+                    <button className="header__btn header__btn--primary">
+                        Become member
+                    </button>
+                </div>
+            </form>
+            {/* Body */}
+            <Carousel className="carousal" responsive={responsive}>
+                <div className="carousal__card">
+                    <div className="carousal__header">
+                        <img src={profile} className="profile" alt="" />
+                        <h2>Member</h2>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam, quod. Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam, quod.
+                    </p>
+                </div>
+                <div className="carousal__card">
+                    <div className="carousal__header">
+                        <img src={profile} className="profile" alt="" />
+                        <h2>Member</h2>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam, quod. Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam, quod.
+                    </p>
+                </div>
+                <div className="carousal__card">
+                    <div className="carousal__header">
+                        <img src={profile} className="profile" alt="" />
+                        <h2>Member</h2>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam, quod. Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam, quod.
+                    </p>
+                </div>
+                <div className="carousal__card">
+                    <div className="carousal__header">
+                        <img src={profile} className="profile" alt="" />
+                        <h2>Member</h2>
+                    </div>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Quisquam, quod. Lorem ipsum dolor sit amet consectetur
+                        adipisicing elit. Quisquam, quod.
+                    </p>
+                </div>
+            </Carousel>
+        </>
     );
 };
 
--- src/Components/Opportunities/Opportunities.js
+++ src/Components/Opportunities/Opportunities.js
@@ -1,52 +1,66 @@
-import { useEffect,useState } from "react"; 
-import mlh from "../../assets/mlh_logo.jpg";
-import gsoc from "../../assets/gsoc_logo.svg";
-import sob from "../../assets/sob_logo.png"
-import {TbLink} from "react-icons/tb"
-import axios from "axios"
-import "./Opportunities.css";
+import { useEffect, useState } from 'react';
+import { TbLink } from 'react-icons/tb';
+import axios from 'axios';
+import './Opportunities.css';
 
 const Opportunities = () => {
-  const [Events , setEvents ] = useState([]);
-  useEffect(() => {
-  const fecthData = async () => {
-    await axios.get("http://localhost:8000/api/opportunities/events")
-        .then(res => {
-            setEvents(res.data.data);
-            console.log(Events);
-        });
-};
-fecthData();
-},[])
-  return (
-    <div className="Opp_body">
-      {/* opportunities heading  */}
-      <div className="Opp_heading">All Upcoming open-source opportunities</div>
-      <div className="Opp_cards_list">
-        {/* for Google summer of code */}
-        {Events && Events.map && Events.map((Event,_id) => <div key={_id} className="Opp_card">
-          <div className="card_left ">
-            <img src={Event.logo} className="event_logo" />
-          </div>
-          <div className="card_right">
-            <span className="card_right_heading">{Event.eventName}</span>
-            <span className="card_right_description">
-              {" "}
-          {Event.description}
-            </span>
-            <div className="card_right_button">
-              <button className="visit_button"> <a
-                rel='noreferrer'
-                href={Event.websiteUrl}
-                target='_blank'
-              ><TbLink/>Visit</a></button>
-              <span className="month_name">{Event.months}</span>
+    const [Events, setEvents] = useState([]);
+    useEffect(() => {
+        const fecthData = async () => {
+            await axios
+                .get('http://localhost:8000/api/opportunities/events')
+                .then((res) => {
+                    setEvents(res.data.data);
+                    console.log(Events);
+                });
+        };
+        fecthData();
+    }, []);
+    return (
+        <div className="Opp_body">
+            {/* opportunities heading  */}
+            <div className="Opp_heading">
+                All Upcoming open-source opportunities
             </div>
-          </div>
-        </div>)}
-      </div>
-    </div>
-  );
+            <div className="Opp_cards_list">
+                {/* for Google summer of code */}
+                {Events &&
+                    Events.map &&
+                    Events.map((Event, _id) => (
+                        <div key={_id} className="Opp_card">
+                            <div className="card_left ">
+                                <img src={Event.logo} className="event_logo" />
+                            </div>
+                            <div className="card_right">
+                                <span className="card_right_heading">
+                                    {Event.eventName}
+                                </span>
+                                <span className="card_right_description">
+                                    {' '}
+                                    {Event.description}
+                                </span>
+                                <div className="card_right_button">
+                                    <button className="visit_button">
+                                        {' '}
+                                        <a
+                                            rel="noreferrer"
+                                            href={Event.websiteUrl}
+                                            target="_blank"
+                                        >
+                                            <TbLink />
+                                            Visit
+                                        </a>
+                                    </button>
+                                    <span className="month_name">
+                                        {Event.months}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+            </div>
+        </div>
+    );
 };
 
 export default Opportunities;
