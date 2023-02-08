fd70f2a961fa4b8dcc554f56ed1dea7052086c4e
lokeshvasnik <lokeshvasnik2003@gmail.com>
2023-02-08 17:19:49 +0530

    Members page completed

--- src/Components/Members/Member.js
+++ src/Components/Members/Member.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import profile from '../../assets/images/profile.png';
 import Carousel from 'react-multi-carousel';
+import gitIcon from '../../assets/images/github.svg';
 import 'react-multi-carousel/lib/styles.css';
 import './Member.css';
 const Member = () => {
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => setModal(!modal);
     const responsive = {
         superLargeDesktop: {
             breakpoint: { max: 4000, min: 3000 },
@@ -24,78 +29,68 @@ const Member = () => {
     };
     return (
         <>
-            <form action="" className="box">
-                <div className="header">
-                    <img src={profile} className="header__profile" alt="" />
-                    <button
-                        type="file"
-                        id="myFile"
-                        name="filename"
-                        className="header__btn header__btn--primary"
-                    >
-                        Change Image
-                    </button>
-                    <button className="header__btn header__btn--secondary">
-                        Remove
-                    </button>
-                </div>
-                <div className="middle">
-                    <input className="input" type="text" placeholder="Name" />
-                    <input
-                        className="input"
-                        type="text"
-                        placeholder="Github Link"
-                    />
-                    <textarea
-                        rows="4"
-                        cols="50"
-                        className="input-textarea"
-                        placeholder="Share your thoughts about Open-source"
-                    ></textarea>
-                    <button className="header__btn header__btn--primary">
-                        Become member
-                    </button>
-                </div>
-            </form>
-            {/* Body */}
+            <div className="heading">
+                <h1>Want to become Openlysource member</h1>
+                <Button color="danger" onClick={toggle}>
+                    Join Now
+                </Button>
+            </div>
+            <div>
+                <Modal isOpen={modal} toggle={toggle}>
+                    <ModalHeader toggle={toggle}>Become a member</ModalHeader>
+                    <ModalBody>
+                        <form action="" className="box">
+                            <div className="header">
+                                <img
+                                    src={profile}
+                                    className="header__profile"
+                                    alt=""
+                                />
+                                <button
+                                    type="file"
+                                    id="myFile"
+                                    name="filename"
+                                    className="header__btn header__btn--primary"
+                                >
+                                    Change Image
+                                </button>
+                                <button className="header__btn header__btn--secondary">
+                                    Remove
+                                </button>
+                            </div>
+                            <div className="middle">
+                                <input
+                                    className="input"
+                                    type="text"
+                                    placeholder="Name"
+                                />
+                                <input
+                                    className="input"
+                                    type="text"
+                                    placeholder="Github Link"
+                                />
+                                <textarea
+                                    rows="4"
+                                    cols="50"
+                                    className="input-textarea"
+                                    placeholder="Share your thoughts about Open-source"
+                                ></textarea>
+                            </div>
+                        </form>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="primary" onClick={toggle}>
+                            Submit
+                        </Button>
+                    </ModalFooter>
+                </Modal>
+            </div>
             <Carousel className="carousal" responsive={responsive}>
                 <div className="carousal__card">
                     <div className="carousal__header">
                         <img src={profile} className="profile" alt="" />
                         <h2>Member</h2>
-                    </div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quisquam, quod. Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Quisquam, quod.
-                    </p>
-                </div>
-                <div className="carousal__card">
-                    <div className="carousal__header">
-                        <img src={profile} className="profile" alt="" />
-                        <h2>Member</h2>
-                    </div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quisquam, quod. Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Quisquam, quod.
-                    </p>
-                </div>
-                <div className="carousal__card">
-                    <div className="carousal__header">
-                        <img src={profile} className="profile" alt="" />
-                        <h2>Member</h2>
-                    </div>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Quisquam, quod. Lorem ipsum dolor sit amet consectetur
-                        adipisicing elit. Quisquam, quod.
-                    </p>
-                </div>
-                <div className="carousal__card">
-                    <div className="carousal__header">
-                        <img src={profile} className="profile" alt="" />
-                        <h2>Member</h2>
+                        <img src={gitIcon} className="icon" alt="github" />
                     </div>
                     <p>
                         Lorem ipsum dolor sit amet consectetur adipisicing elit.
