90514f8195c68246fba4b1f39b9aff8e8112c768
Harsh <hparashar617@gmail.com>
2023-02-21 21:54:30 +0530

    add menutoggler to navbar mobile view

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from 'react';
 import { BsGithub } from "react-icons/bs";
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import "./Navbar.css";
 import logo from "../../assets//Official_openlySource_logo.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <>
       <div className="gradient_bg">
@@ -33,19 +35,38 @@ const Navbar = () => {
                   GitHub <BsGithub />
                 </a>
               </li>
-              {/* <li>
-                                <Link to="/Sign-up">
-                                    <button class="btn">Sign Up</button>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link to="/Log-in">
-                                    <button class="btn">Log in</button>
-                                </Link>{' '}
-                            </li> */}
             </ul>
           </div>
         </div>
+        <div className="navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine classname="menu_icon" color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line classname="menu_icon" color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+        {toggleMenu && (
+         <div className="list_container_mobile">
+         <ul className="Navbar_list_mobile">
+           <li>
+             <Link to="/Aboutus">About us</Link>
+           </li>
+           <li>
+             <Link to="/Members">Our Members</Link>
+           </li>
+           <li>
+             <Link to="/Opportunities">Opportunities</Link>
+           </li>
+           <li>
+             <a
+               rel="noreferrer"
+               href="https://github.com/hparashar27/openlysource"
+               target="_blank"
+             >
+               GitHub <BsGithub />
+             </a>
+           </li>
+         </ul>
+       </div>
+        )}
+      </div>
       </div>
     </>
   );
