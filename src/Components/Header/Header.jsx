227bd4bd4e0bcf7bf4a619f2f541f41da2bee1c4
Harsh <hparashar617@gmail.com>
2023-03-02 20:16:46 +0530

    Improve landing page heading

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 <div className='main'>
         <video src={Header_video_} autoPlay loop muted />
         <div className="content">
-        <h2>Connecting the Open Source Community</h2>
+        <p>Connecting the Open Source Community</p>
         <h4> A Platform for Open Source Collaboration and Growth ,where one can share open source experience and Learn from the Pros</h4>
         <h3>Connect, Learn, and Contribute</h3>
         <div className='slack_sec'>
